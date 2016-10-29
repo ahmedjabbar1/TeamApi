87a2ee4d16e2249d4349f516dbd4e59c0544b568
ahmedjabbar1 <ahmedjabbarkasad@gmail.com>
2016-10-29 13:33:51 +0300

    Add files via upload

---
+++ plugins/report.lua
@@ -0,0 +1,54 @@
+local plugin = {}
+
+local function report(msg, description)
+    local text = _('• *Message reported by*: %s (`%d`)\n• *Group*: %s'):format(misc.getname_final(msg.from), msg.from.id, msg.chat.title:escape())
+    if msg.reply.sticker then
+        text = text.._('\n• *Sticker sent by*: %s (`%d`)'):format(misc.getname_final(msg.reply.from), msg.reply.from.id)
+    end
+    if msg.chat.username then
+        text = text.._('\n• [Go to the message](%s)'):format('telegram.me/'..msg.chat.username..'/'..msg.message_id)
+    end
+    if description then
+        text = text.._('\n• *Description*: %s'):format(description:escape(true))
+    end
+    
+    local res = api.getChatAdministrators(msg.chat.id)
+    if not res then return false end
+    
+    local n = 0
+    for i, admin in pairs(res.result) do
+        local receive_reports = db:hget('user:'..admin.user.id..':settings', 'reports')
+        if receive_reports and receive_reports == 'on' then
+            local res_fwd = api.forwardMessage(admin.user.id, msg.chat.id, msg.reply.message_id)
+            if res_fwd then
+                api.sendMessage(admin.user.id, text, true, nil, nil, res_fwd.result.message_id)
+                n = n + 1
+            end
+        end
+    end 
+    return n
+end
+
+function plugin.onTextMessage(msg, blocks)
+    if msg.chat.type == 'private' or roles.is_admin_cached(msg) or not msg.reply then return end
+    if roles.is_admin_cached(msg.reply) then return end
+    local status = (db:hget('chat:'..msg.chat.id..':settings', 'Reports')) or config.chat_settings['settings']['Reports']
+    if not status or status == 'off' then return end
+    
+    local n_sent = report(msg, blocks[1])
+    if n_sent then
+        local text = _('_Reported to %d admin(s)_'):format(n_sent)
+        api.sendReply(msg, text, true)
+    end
+end
+
+plugin.triggers = {
+    onTextMessage = {
+        '^@admin$',
+        '^@admin (.*)',
+        config.cmd..'report$',
+        config.cmd..'report (.*)',
+    }
+}
+
+return plugin
